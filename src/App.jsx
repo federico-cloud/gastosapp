f351c1eea972f87e32a4e946d0967bf6ba0ed360
Federico Galan <federico.egv@gmail.com>
2025-01-25 10:02:38 -0300

    Cambios minimos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { Routes, Route } from 'react-router-dom'; // Importa los componentes de rutas
-import { Home } from './pages/Home'; // Página principal
-import { Dashboard } from './pages/Dashboard'; // Panel de control
+import { Routes, Route } from 'react-router-dom';
+import { Dashboard } from './layouts/DashboardLayout';
+import { Home } from './pages';
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} /> {/* Ruta para la página principal */}
-      <Route path="/dashboard" element={<Dashboard />} /> {/* Ruta para el Dashboard */}
+      <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 };
