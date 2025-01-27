b66e6ec2fc908be56dbc368aea8a8bef2958302d
Federico Galan <federico.egv@gmail.com>
2025-01-27 00:19:30 -0300

    Componente: se agrega el componente Outlet dentro del dashboard layout para que renderice las rutas dentro del mismo

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,5 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import { Sidebar } from '../components/Sidebar';
+
 export const DashboardLayout = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className='bg-amber-300 w-full h-full'>
+      <Sidebar />
+      <main className="flex-grow p-6">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
