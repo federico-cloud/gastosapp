5990a8044999b4321812eab07030d842f4345349
Federico Galan <fegv.2@hotmail.com.ar>
2025-01-31 12:47:00 -0300

    Estilos: cambios minimos

--- src/pages/ManageExpenses.jsx
+++ src/pages/ManageExpenses.jsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom";
+
 export const ManageExpenses = () => {
+  const expenses = [
+    { id: 1, name: "Compra en supermercado", category: "Super", amount: "$50" },
+    { id: 2, name: "Gasolina", category: "automovil", amount: "$30" },
+  ];
+
   return (
-    <div>
+    <div className="p-4">
       <h1>Gestionar Gastos</h1>
-      <p>Esta es la página donde se gestionan los gastos.</p>
+      <ul>
+        {expenses.map((expense) => (
+          <li key={expense.id} className="p-2 my-2 border">
+            <Link
+              to={`/dashboard/manage-expenses/${expense.id}`}
+            >
+              <p>Detalle: {expense.name}</p>
+              <p>Importe: {expense.amount}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
