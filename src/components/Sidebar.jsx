acb89fd453d83cefb80638c88c4ea41a6a11ea7a
Federico Galan <fegv.2@hotmail.com.ar>
2025-01-31 12:36:04 -0300

    Enrutamiento y estilos: se modifican rutas de componentes y estilos

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,21 +5,24 @@ export const Sidebar = () => {
   const [activeLink, setActiveLink] = useState(null);
 
   return (
-    <aside className="w-64 h-screen text-white bg-gray-800">
+    <aside className="fixed top-0 left-0 w-64 h-screen text-white bg-gray-800">
       <nav>
         <ul>
           <SidebarLink
             text="Panel principal"
+            href="/dashboard"
             isActive={activeLink === "Panel principal"}
             onClick={() => setActiveLink("Panel principal")}
           />
           <SidebarLink
             text="Gestionar gastos"
+            href="/dashboard/manage-expenses"
             isActive={activeLink === "Gestionar gastos"}
             onClick={() => setActiveLink("Gestionar gastos")}
           />
           <SidebarLink
             text="Gestionar ingresos"
+            href=""
             isActive={activeLink === "Gestionar ingresos"}
             onClick={() => setActiveLink("Gestionar ingresos")}
           />
