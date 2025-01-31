d1adf148c807a9e13553665d52edf64c3a6de212
Federico Galan <fegv.2@hotmail.com.ar>
2025-01-31 12:46:49 -0300

    Componentes: Se modifica el Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,10 @@
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-wrap justify-around p-4 h-full">
+      <div className="w-1/4 h-1/4 mb-12 text-center bg-amber-700">Panel</div>
+      <div className="w-1/4 h-1/4 mb-12 text-center bg-amber-700">Panel</div>
+      <div className="w-1/4 h-1/4 mb-12 text-center bg-amber-700">Panel</div>
+      <div className="w-2/3 h-1/2 m-4 text-center bg-amber-700">Panel</div>
+    </div>
+  );
 };
