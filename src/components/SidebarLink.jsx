a945ba1ff07d93a149369f3f326381645a74d324
Federico Galan <fegv.2@hotmail.com.ar>
2025-01-31 12:36:44 -0300

    Componentes: Se agrega el atributo href a los links

--- src/components/SidebarLink.jsx
+++ src/components/SidebarLink.jsx
@@ -1,11 +1,11 @@
-export const SidebarLink = ({ text, isActive, onClick }) => {
+export const SidebarLink = ({ text, href, isActive, onClick }) => {
   return (
     <li>
       <a
         className={`inline-block w-full py-4 text-center transition duration-400 ${
           isActive ? "bg-gray-700 border-r-4" : "hover:bg-gray-700"
         }`}
-        href="#"
+        href={href}
         onClick={onClick}
       >
         {text}
