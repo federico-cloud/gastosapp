36a4432de139849a278138be61ab8b7936dc2fe5
Federico Galan <fegv.2@hotmail.com.ar>
2025-02-06 17:29:51 -0300

    Componentes: se modifica la pagina home del dashboard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
+import { MonthExpenses } from "../components";
+import { v4 as uuidv4 } from 'uuid';
+
 export const Home = () => {
+  const months = [
+    {id: uuidv4(), month:"Enero"},
+    {id: uuidv4(), month:"Febrero"},
+    {id: uuidv4(), month:"Marzo"},
+    {id: uuidv4(), month:"Abril"},
+    {id: uuidv4(), month:"Mayo"},
+    {id: uuidv4(), month:"Junio"},
+    {id: uuidv4(), month:"Julio"},
+    {id: uuidv4(), month:"Agosto"},
+    {id: uuidv4(), month:"Septiembre"},
+    {id: uuidv4(), month:"Octubre"},
+    {id: uuidv4(), month:"Noviembre"},
+    {id: uuidv4(), month:"Diciembre"},
+  ];
+
   return (
-    <div className="flex flex-wrap justify-around p-4 h-full">
-      <div className="w-1/4 h-1/4 mb-12 text-center bg-amber-700">Panel</div>
-      <div className="w-1/4 h-1/4 mb-12 text-center bg-amber-700">Panel</div>
-      <div className="w-1/4 h-1/4 mb-12 text-center bg-amber-700">Panel</div>
-      <div className="w-2/3 h-1/2 m-4 text-center bg-amber-700">Panel</div>
-    </div>
+    <>
+      {months.map(({id, month}) => (
+        <MonthExpenses key={id} month={month} />
+      ))}
+    </>
   );
 };
