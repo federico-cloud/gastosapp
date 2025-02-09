b02e6bad06f9840ddd8fde9ea63a16f8f895ff3e
Federico Galan <federico.egv@gmail.com>
2025-02-09 20:09:23 -0300

    Componente: se modifican estilos y se agrega un uuid temporal a los meses del home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,28 @@
 import { MonthExpenses } from "../components";
 import { useMonths } from "../hooks";
+import { v4 as uuidv4 } from "uuid";
 
 export const Home = () => {
   const { months, loading, error } = useMonths();
   if (loading) return <p>Cargando...</p>;
   if (error) return <p>Error al cargar los datos: {error.message}</p>;
 
+  const monthsWithId = months.map((month) => ({
+    ...month,
+    tempId: uuidv4(),
+  }));
+
   return (
-    <>
-      {months.map(({ id, month, totalDays }) => (
-        <MonthExpenses key={id} month={month} totalDays={totalDays} />
+    <div className="flex flex-row flex-wrap justify-center">
+      {monthsWithId.map(({ tempId, yearId, monthId, month, totalDays }) => (
+        <MonthExpenses
+          key={tempId}
+          yearId={yearId}
+          monthId={monthId}
+          month={month}
+          totalDays={totalDays}
+        />
       ))}
-    </>
+    </div>
   );
 };
