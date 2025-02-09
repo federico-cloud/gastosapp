566e360c536b1f48b49909d087b66048d7a3960d
Federico Galan <federico.egv@gmail.com>
2025-02-09 20:11:39 -0300

    Estilos: se hacen cambios minimos

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -3,9 +3,9 @@ import { Sidebar } from "../components/Sidebar";
 
 export const DashboardLayout = () => {
   return (
-    <div className="flex w-full h-screen">
+    <div className="flex w-full h-full">
       <Sidebar />
-      <main className="w-full ml-64">
+      <main className="w-full ml-64 bg-darkpurple-500">
         <Outlet />
       </main>
     </div>
