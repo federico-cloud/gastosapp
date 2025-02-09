1305c3c1ca0b8908daeb12795cdfdc54566b01ae
Federico Galan <federico.egv@gmail.com>
2025-02-09 20:06:01 -0300

    Enrutamiento: se agrega ruta para ver detalles de gastos mensuales

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { RouterAdapter } from "./adapters/";
+import { DetailsMonthExpenses } from "./components";
 import { DashboardLayout } from "./layouts/";
 import { Home, ManageExpenses } from "./pages";
 
@@ -9,9 +10,13 @@ export const App = () => {
       element: <DashboardLayout />,
       children: [
         // Ruta index ("/dashboard")
-        { path: "", element: <Home/> },
+        { path: "", element: <Home /> },
         // Ruta "/dashboard/manage-expenses"
         { path: "manage-expenses", element: <ManageExpenses /> },
+        {
+          path: "view-expenses/:year/:month",
+          element: <DetailsMonthExpenses />,
+        },
       ],
     },
   ];
