fb2c114eb0949e9c3b0c0d2881629244cd9c8b15
Federico Galan <fegv.2@hotmail.com.ar>
2025-02-12 17:41:53 -0300

    Componentes: se modifica el componente

--- src/components/DetailsMonthExpenses.jsx
+++ src/components/DetailsMonthExpenses.jsx
@@ -1,8 +1,19 @@
 import { useParams } from "react-router-dom";
+import { useExpensesByYear } from "../hooks/useExpensesByYear";
 
 export const DetailsMonthExpenses = () => {
   const { year, month } = useParams();
+  const { expensesByYear, loading, error } = useExpensesByYear({ year });
 
+  const expensesByMonth = expensesByYear.filter(
+    (expense) => expense.monthId === month
+  );
 
-  return <h1 className="text-white">Detalles del Mes {month} y ano {year}</h1>;
+  console.log(expensesByMonth)
+
+  return (
+    <h1 className="text-white">
+      Detalles del Mes {month} y ano {year}
+    </h1>
+  );
 };
