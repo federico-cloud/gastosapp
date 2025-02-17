19eba8cd296f571a43ac9b28fd816a56f532ea16
Federico Galan <fegv.2@hotmail.com.ar>
2025-02-17 17:02:41 -0300

    Hooks: se crea el hook para obtener los gastos por mes

---
+++ src/hooks/useExpensesByMonth.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { expensesService } from "../api";
+
+export const useExpensesByMonth = ({ year, month }) => {
+  const [expensesByMonth, setExpensesByMonth] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchExpensesByMonth = async () => {
+      try {
+        const data = await expensesService.getExpensesByMonth(year, month);
+        console.log(data)
+        setExpensesByMonth(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchExpensesByMonth(year, month);
+  }, [year, month]);
+
+  return { expensesByMonth, loading, error };
+};
