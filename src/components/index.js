cfb41fb2fb81c9bb63422b4f0b8841cabaf4afba
Federico Galan <fegv.2@hotmail.com.ar>
2025-02-21 17:02:48 -0300

    Componentes: se modifica el archivo barril

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { SidebarLink } from "./SidebarLink";
 export { SidebarProfile } from "./SidebarProfile";
 export { MonthExpenses } from "./MonthExpenses";
 export { DetailsMonthExpenses } from "./DetailsMonthExpenses";
+export { TableMonthExpenses } from "./TableMonthExpenses";
