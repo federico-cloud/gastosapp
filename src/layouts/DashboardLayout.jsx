d0a3758750b054b1030b04fe4c85a50289f019ef
Federico Galan <fegv.2@hotmail.com.ar>
2025-02-27 11:28:05 -0300

    Estilos: se modifican estilos del componente

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -15,17 +15,19 @@ export const DashboardLayout = () => {
     <div className="flex w-full h-full p-2">
       {displayMenu ? (
         <MdClose
-          className="fixed z-10 m-auto text-4xl text-white hover:cursor-pointer"
+          className="fixed z-10 p-2 text-white h-14 w-14 m-autotext-4xl top-6 left-6 hover:cursor-pointer hover:text-turqo-600"
           onClick={handleMenu}
         />
       ) : (
         <RxHamburgerMenu
-          className="fixed z-10 m-auto text-4xl text-white hover:cursor-pointer"
+          className="fixed z-10 p-2 m-auto text-4xl text-white bg-purple-500 h-14 w-14 top-6 left-6 hover:cursor-pointer hover:text-turqo-600"
           onClick={handleMenu}
         />
       )}
       <Sidebar displayMenu={displayMenu} />
-      <main className="w-full ml-64 bg-darkpurple-500">
+      <main
+        className={`w-full ${displayMenu ? "ml-64" : ""} bg-darkpurple-500`}
+      >
         <Outlet />
       </main>
     </div>
