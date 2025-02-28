e38f6fd3461dd81719ccd3d0378ceb898b88c87d
Federico Galan <fegv.2@hotmail.com.ar>
2025-02-28 15:58:57 -0300

    Estilos: se modifican estilos del componente

--- src/components/SidebarLink.jsx
+++ src/components/SidebarLink.jsx
@@ -1,11 +1,10 @@
 import { NavLinkAdapter } from "../adapters/RouterAdapter";
-import { IoExitOutline } from "react-icons/io5";
 
 export const SidebarLink = ({ text, href, onClick, activeLink, icon }) => {
   return (
-    <li className={`flex items-center w-full ${
-          activeLink === text ? "border-l-4 bg-turqo-600" : "hover:bg-gray-600 "
-        } hover:text-turqo-600 hover:cursor-pointer`}>
+    <li className={`flex items-center w-full p-2 ${
+          activeLink === text ? "border-l-4 bg-turqo-600" : "hover:bg-gray-600 text-white"
+        } hover:cursor-pointer`}>
       <NavLinkAdapter
         className={`text-center transition duration-400 px-2 py-4 w-full text-xl`}
         to={href}
